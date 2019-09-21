edb89560cbd18655e6269e03cd034c8d95356ff2
Ki Jung Kim <kijungkim@eworldes.com>
2019-09-20 14:10:29 -1000

    refactored and cleaned up webhook route

--- routes/webhook/index.js
+++ routes/webhook/index.js
@@ -1,54 +1,50 @@
 const
-  express = require('express');
+  express = require('express'),
+  httpStatusCodes = require('../../utilities/constants/http-status-codes');
 
 const
   router = express.Router();
 
-// Creates the endpoint for our webhook
 router.route('/')
   .get((request, response) => {
-    // Your verify token. Should be a random string.
     const webhookVerificationToken = process.env.WEBHOOK_VERIFICATION_TOKEN;
 
-    // Parse the query params
     const
       mode = request.query['hub.mode'],
       token = request.query['hub.verify_token'],
       challenge = request.query['hub.challenge'];
 
-    // Checks if a token and mode is in the query string of the request
     if (!mode || !token) {
-      return response.sendStatus(401)
+      return response.sendStatus(httpStatusCodes.unauthorized)
     }
 
-    // Checks the mode and token sent is correct
     if (mode !== 'subscribe' || token !== webhookVerificationToken) {
-      return response.sendStatus(403);
+      return response.sendStatus(httpStatusCodes.forbidden);
     }
 
-    // Passed all checks, send back the challenge token
-    return response.status(200).send(challenge);
+    return response.status(httpStatusCodes.ok).send(challenge);
   })
 
   .post((request, response) => {
     const body = request.body;
 
-    // Gate check for all requests that are not from a page subscription
     if (body.object !== 'page') {
-      return response.sendStatus(404);
+      return response.sendStatus(httpStatusCodes.notFound);
     }
 
-    // Iterates over each entry - there may be multiple if batched
     body.entry.forEach((entry) => {
+      const
+        entryId = entry.id, // the page entry id
+        event = entry.messaging[0], // the webhook event
+        senderId = event.sender.id; // the page-scoped id of event sender
 
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      const webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      console.log('entryId: ', entryId);
+      console.log('event: ', event);
+      console.log('senderId: ', senderId);
     });
 
     // Returns a '200 OK' response to all requests
-    return response.status(200).send('EVENT_RECEIVED');
+    return response.sendStatus(httpStatusCodes.ok);
   });
 
 module.exports = router;
