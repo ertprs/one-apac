6f03f7121ac402ba4b32b3e746548b2e09e5b150
Ki Jung Kim <kijungkim@eworldes.com>
2019-09-29 21:16:51 -1000

    hotfix db

--- db/migrations/20190929210721_alter_views_table.js
+++ db/migrations/20190929210721_alter_views_table.js
@@ -1,14 +1,14 @@
 
 exports.up = function(knex) {
   return knex.schema.alterTable('views', (table) => {
-    table.dropUnique('description');
-    table.integer('event_id').notNullable();
+    table.string('description').notNullable().alter();
+    table.integer('event_id').notNullable().alter();
   });
 };
 
 exports.down = function(knex) {
   return knex.schema.alterTable('views', (table) => {
-    table.string('description').unique();
-    table.integer('event_id').nullable();
+    table.string('description').unique().alter();
+    table.integer('event_id').nullable().alter();
   });
 };
