c4910346326334605e2d8886f7d1475bfd67c086
Ki Jung Kim <kijungkim@eworldes.com>
2019-09-30 13:12:06 -1000

    added administrators routes

---
+++ routes/administrators/index.js
@@ -0,0 +1,62 @@
+const
+  express = require('express'),
+  { hashPassword } = require('../../utilities/authentication/hash-password'),
+  httpStatusCodes = require('../../utilities/constants/http-status-codes'),
+  queries = require('../../db/queries');
+
+const router = express.Router();
+
+router.route('/')
+  .post((request, response) => {
+    const
+      { body } = request,
+      { username, eventId } = body;
+
+    let { password } = body;
+
+    password = hashPassword(password);
+
+    return queries.administrators.insertAdministrator(username, password, eventId)
+      .then(() => {
+        console.log('Administrator created');
+        return response.sendStatus(httpStatusCodes.ok);
+      })
+      .catch((error) => {
+        return queries.errors.logError(error.name, error.message, error.stack);
+      });
+  });
+
+router.route('/login')
+  .post((request, response) => {
+    const
+      { body } = request,
+      { username } = body;
+
+    let { password } = body;
+
+    password = hashPassword(password);
+
+    return queries.administrators.getAdministrator(username)
+      .then((result) => {
+        const administrator = result.rows[0];
+
+        if (!administrator) {
+          const usernameError = new Error('Incorrect username');
+          usernameError.status = 400;
+          throw usernameError;
+        }
+
+        if (administrator.password !== password) {
+          const passwordError = new Error('Incorrect password');
+          passwordError.status = 400;
+          throw passwordError;
+        }
+
+        return response.status(httpStatusCodes.ok).json(administrator);
+      })
+      .catch((error) => {
+        queries.errors.logError(error.name, error.message, error.stack);
+
+        return response.status(error.status).send(error.message);
+      });
+  });
