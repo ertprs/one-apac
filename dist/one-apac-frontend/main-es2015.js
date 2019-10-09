ff2cd3f25e2e888bf7c94e3ecc362ce351848318
Ki Jung Kim <kijungkim@eworldes.com>
2019-10-08 16:18:55 -1000

    wip login

--- dist/one-apac-frontend/main-es2015.js
+++ dist/one-apac-frontend/main-es2015.js
@@ -45,7 +45,7 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony default export */ __webpack_exports__["default"] = ("<div class=\"component\" id=\"alert\">\n  {{alert.message}}\n</div>");
+/* harmony default export */ __webpack_exports__["default"] = ("<div class=\"component\" id=\"alert\">\n  {{alert}}\n</div>");
 
 /***/ }),
 
