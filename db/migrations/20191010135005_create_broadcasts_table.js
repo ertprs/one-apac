76ae1e6f0432098fdd096647e3adb8769e0d9f8a
Ki Jung Kim <kijungkim@eworldes.com>
2019-10-10 19:52:57 -1000

    hotfix typo

--- db/migrations/20191010135005_create_broadcasts_table.js
+++ db/migrations/20191010135005_create_broadcasts_table.js
@@ -5,7 +5,7 @@ exports.up = function(knex) {
     table.string('description', 256).notNullable().unique();
     table.string('message', 2048).notNullable();
     table.integer('event_id');
-    table.foreign('event_id').references('id').inTable('event');
+    table.foreign('event_id').references('id').inTable('events');
     table.timestamps(true, true);
   });
 };
