1e38c9c1407af21e46cf39f221b75617d43ec28a
Ki Jung Kim <kijungkim@eworldes.com>
2019-10-14 14:54:58 -1000

    created views and votes routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const
   express = require('express'),
   administrators = require('./administrators'),
   broadcasts = require('./broadcasts'),
+  views = require('./views'),
+  votes = require('./votes'),
   webhook = require('./webhook');
 
 const
@@ -9,6 +11,8 @@ const
 
 router.use('/administrators', administrators);
 router.use('/broadcasts', broadcasts);
+router.use('/views', views);
+router.use('/votes', votes);
 router.use('/webhook', webhook);
 
 module.exports = router;
