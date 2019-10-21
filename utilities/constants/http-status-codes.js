60a0005bceeaacac3184c70b0a709aaa7e1e3fe6
Ki Jung Kim <kijungkim@eworldes.com>
2019-10-21 10:10:44 -1000

    added a constant for internal server error code

--- utilities/constants/http-status-codes.js
+++ utilities/constants/http-status-codes.js
@@ -3,7 +3,8 @@ const httpStatusCodes = {
   badRequest: 400,
   unauthorized: 401,
   forbidden: 403,
-  notFound: 404
+  notFound: 404,
+  internalServer: 500
 };
 
 module.exports = httpStatusCodes;
